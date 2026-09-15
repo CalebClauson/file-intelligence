cf7bf642e13fd379d4ca9411a612ef3589d21a5b
CalebClauson <calebjr.2@gmail.com>
2026-09-15 00:23:06 -0400

    Unit Tests

---
+++ tests/test_scanner.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+import json
+
+from scanner import (format_size, scan_directory, get_file_info, count_files, total_size, count_by_extension, get_largest_files, group_by_size, filter_duplicates, find_duplicates, find_empty_directories, generate_report, json_dump,)
+
+def test_format_size_bytes():
+    pass
+
+
+def test_format_size_kilobytes():
+    pass
+
+
+def test_count_files():
+    pass
+
+
+def test_count_by_extension():
+    pass
+
+
+def test_count_by_extension_no_extension():
+    pass
+
+
+def test_scan_directory_finds_nested_files():
+    pass
+
+
+def test_get_file_info():
+    pass
+
+
+def test_total_size():
+    pass
+
+
+def test_group_by_size():
+    pass
+
+
+def test_filter_duplicates():
+    pass
+
+
+def test_find_duplicates():
+    pass
+
+
+def test_find_empty_directories():
+    pass
+
+
+def test_get_largest_files():
+    pass
+
+
+def test_generate_report_contains_expected_keys():
+    pass
+
+
+def test_json_export_creates_report_file():
+    pass
